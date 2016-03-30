fc3795ce60cd95691cadcd93983f101d1b6f4529
mattman00000 <mattman00000@gmail.com>
2016-03-30 02:23:31 -0700

    0.0.2 The Persistence Update

--- youtube_playlist_manipulation_helper.user.js
+++ youtube_playlist_manipulation_helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Playlist Manipulation Helper
 // @namespace    https://github.com/mattman00000
-// @version      0.0.1
+// @version      0.0.2
 // @description  Allows you to set and then go to positions on the page for moving multiple videos from the top or bottom of a playlist to a desired arbitrary point somewhere in the middle. Coming soon, maybe, movement between two points. Don't hold your breath for more than two. Works best with either two working hands, one to operate the mouse to drag videos and the other to press home/end/insert/delete, or with one working hand and a mouse that has reprogrammable buttons like just about any G-series (and most others) Logitech gaming mouse.
 // @author       mattman00000
 // @match        https://www.youtube.com/playlist*
@@ -12,12 +12,12 @@
 	'use strict';
 
 	// Your code here...
-	var scry = 0;
+	if (localStorage.getItem("scry")==0) localStorage.setItem("scry","0");
 	window.addEventListener("keydown",function(a){
 		if (a.key == "Delete") {
-			scry = window.scrollY;
+			localStorage.setItem("scry",window.scrollY);
 		} else if (a.key == "Insert") {
-			window.scrollTo(0,scry);
+			window.scrollTo(0,Number(localStorage.getItem("scry")));
 		}
 	});
 })();
